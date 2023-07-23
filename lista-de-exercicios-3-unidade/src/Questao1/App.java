c77dc1fa6423eb4b924e54f4efa1b0312781f039
Mariana Oliveira Moraes <110694719+marianaoliveiram@users.noreply.github.com>
2023-07-23 12:16:24 -0300

    resolvendo questões
    Iniciei mas não consegui....

---
+++ lista-de-exercicios-3-unidade/src/Questao1/App.java
@@ -0,0 +1,7 @@
+package Questao1;
+
+public class App{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ lista-de-exercicios-3-unidade/src/Questao1/Jogador.java
@@ -0,0 +1,17 @@
+package Questao1;
+
+/*public class Jogador {
+    private String nome;
+    private int numero;
+    private String posicao;
+    private boolean lesao;
+    private int quantCartoes;
+    private boolean expulsao;
+
+    public enum TipoPosicao(){
+        GOLEIRO (1), 
+        DEFENSOR (2),
+        MEIOCAMPISTA, 
+        ATACANTE;
+    }
+}*/
